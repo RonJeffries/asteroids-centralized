47f332c3935d1d5f119fd25e1139c0ab1f1843de
Ron Jeffries <ronjeffries@acm.org>
2023-01-03 20:07:50 -0500

    sort U constants as close to alpha as possible

--- src/main/kotlin/com/ronjeffries/ship/Universe.kt
+++ src/main/kotlin/com/ronjeffries/ship/Universe.kt
@@ -8,34 +8,37 @@ typealias Velocity = Vector2
 typealias Acceleration = Vector2
 
 object U {
+    const val ASTEROID_SPEED = 100.0
+    const val DRAW_SCALE = 1.0
+    const val DROP_SCALE = 3.0
+    const val FONT_SIZE = 64.0
+    const val INITIAL_ASTEROID_COUNT = 4
     const val KILL_ASTEROID = 64.0
-    const val SCALE_ASTEROID = 4.0
     const val KILL_MISSILE = 1.0
     const val KILL_SAUCER = 10.0 // scaled dx=40 dy=24 suggests 12.0 Make it hard.
-    const val SCALE_SAUCER = 4.0
     const val KILL_SHIP = 12.0
+    const val MAKER_DELAY = 3.0
+    const val MISSILE_LIFETIME = 3.0
+    const val SAUCER_LIFETIME = 10.0
+    const val SAUCER_SPEED = 150.0
+    const val SCALE_ASTEROID = 4.0
+    const val SCALE_SAUCER = 4.0
     const val SCALE_SHIP = 2.0
+    const val SHIPS_PER_QUARTER = 4
+    const val SHIP_ROTATION_SPEED = 200.0 // degrees per second
+    const val SPEED_OF_LIGHT = 500.0
+    const val SPLAT_LIFETIME = 2.0
     const val STROKE_ALL = 1.0
-    const val DRAW_SCALE = 1.0
     const val UNIVERSE_SIZE = 1024.0
     const val WINDOW_SIZE = 1024
-    const val FONT_SIZE = 64.0
-    const val ASTEROID_SPEED = 100.0
-    const val DROP_SCALE = 3.0
-    const val INITIAL_ASTEROID_COUNT = 4
-    const val MAKER_DELAY = 3.0
-    const val SAFE_SHIP_DISTANCE = UNIVERSE_SIZE/10.0
-    const val SAUCER_SPEED = 150.0
-    const val SAUCER_LIFETIME = 10.0
-    const val SPEED_OF_LIGHT = 500.0
-    const val MISSILE_LIFETIME = 3.0
+
     const val MISSILE_SPEED = SPEED_OF_LIGHT / 3.0
-    const val SPLAT_LIFETIME = 2.0
-    const val SHIP_ROTATION_SPEED = 200.0 // degrees per second
-    const val SHIPS_PER_QUARTER = 4
+    const val SAFE_SHIP_DISTANCE = UNIVERSE_SIZE/10.0
+
+    val CENTER_OF_UNIVERSE = Point(UNIVERSE_SIZE / 2, UNIVERSE_SIZE / 2)
     val SHIP_ACCELERATION = Velocity(120.0, 0.0)
     val SHIP_DECELERATION_FACTOR = 0.5 // speed reduces in half every second
-    val CENTER_OF_UNIVERSE = Point(UNIVERSE_SIZE / 2, UNIVERSE_SIZE / 2)
+
     fun randomPoint() = Point(Random.nextDouble(0.0, UNIVERSE_SIZE), Random.nextDouble(0.0, UNIVERSE_SIZE))
     fun randomInsidePoint() = Point(randomInsideDouble(), randomInsideDouble())
     private fun randomInsideDouble() = UNIVERSE_SIZE/10.0 + Random.nextDouble(UNIVERSE_SIZE-2* UNIVERSE_SIZE/10.0)
