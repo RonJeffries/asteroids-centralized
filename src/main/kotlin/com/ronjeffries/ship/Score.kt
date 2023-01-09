215a3624004beb5b95b52186e4d20031748b64aa
Ron Jeffries <ronjeffries@acm.org>
2023-01-09 09:08:41 -0500

    Score implements SpaceObject, not InteractingSpaceObject. Has no behavior at all.

--- src/main/kotlin/com/ronjeffries/ship/Score.kt
+++ src/main/kotlin/com/ronjeffries/ship/Score.kt
@@ -1,7 +1,4 @@
 package com.ronjeffries.ship
 
-class Score(val score: Int): SpaceObject, InteractingSpaceObject {
-    override val subscriptions = Subscriptions()
-    override fun callOther(other: InteractingSpaceObject, trans: Transaction) {}
-    override fun update(deltaTime: Double, trans: Transaction) { }
+class Score(val score: Int): SpaceObject {
 }
