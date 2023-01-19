ab7690eb5130ac8aa793efbedaad0628136fceaf
Ron Jeffries <ronjeffries@acm.org>
2023-01-19 10:42:20 -0500

    make `hit` private.

--- src/main/kotlin/com/ronjeffries/ship/Collision.kt
+++ src/main/kotlin/com/ronjeffries/ship/Collision.kt
@@ -1,7 +1,7 @@
 package com.ronjeffries.ship
 
 class Collision(private val collider: Collider) {
-    fun hit(other: Collider): Boolean
+    private fun hit(other: Collider): Boolean
         = collider.position.distanceTo(other.position) < collider.killRadius + other.killRadius
 
     fun executeOnHit(other: Collider, action: () -> Unit) {
