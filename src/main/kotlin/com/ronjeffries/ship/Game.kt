8001b9ae99722ac1554846ea9838a7b994eda42c
Ron Jeffries <ronjeffries@acm.org>
2023-01-26 10:16:40 -0500

    remove numberOfAsteroidsToCreate member variable.

--- src/main/kotlin/com/ronjeffries/ship/Game.kt
+++ src/main/kotlin/com/ronjeffries/ship/Game.kt
@@ -4,7 +4,6 @@ import org.openrndr.draw.Drawer
 
 class Game() {
     private var lastTime = 0.0
-    private var numberOfAsteroidsToCreate = 0
     private var cycler: GameCycler = GameCycler(SpaceObjectCollection(), 0, Controls())
 
     fun currentMix(): SpaceObjectCollection = cycler.currentMix()
@@ -18,7 +17,6 @@ class Game() {
     }
 
     private fun initializeGame(controls: Controls, shipCount: Int) {
-        numberOfAsteroidsToCreate = U.ASTEROID_STARTING_COUNT
         val knownObjects = SpaceObjectCollection()
         knownObjects.performWithTransaction { trans ->
             createInitialObjects(trans,shipCount, controls, knownObjects)
@@ -32,7 +30,7 @@ class Game() {
         knownObjects: SpaceObjectCollection
     ) {
         knownObjects.scoreKeeper = ScoreKeeper(shipCount)
-        cycler = GameCycler(knownObjects, numberOfAsteroidsToCreate, controls)
+        cycler = GameCycler(knownObjects, U.ASTEROID_STARTING_COUNT, controls)
         trans.clear()
     }
 
