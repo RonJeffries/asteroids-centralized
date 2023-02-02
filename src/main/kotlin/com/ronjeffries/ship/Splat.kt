091170170fe132d3e0921a2384caae1485a2dea7
Ron Jeffries <ronjeffries@acm.org>
2023-02-02 14:04:26 -0500

    Splat is a collider that collides with nothing.

--- src/main/kotlin/com/ronjeffries/ship/Splat.kt
+++ src/main/kotlin/com/ronjeffries/ship/Splat.kt
@@ -4,19 +4,29 @@ import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.Drawer
 
 class Splat(
-    var position: Point,
+    override var position: Point,
     val scale: Double = 1.0,
     val color: ColorRGBa = ColorRGBa.WHITE,
     val velocity: Velocity = Velocity.ZERO
-) : SpaceObject {
+) : SpaceObject, Collidable, Collider {
     constructor(ship: Ship) : this(ship.position, 2.0, ColorRGBa.WHITE, ship.velocity*0.5)
     constructor(missile: Missile) : this(missile.position, 0.5, missile.color, missile.velocity*0.5)
     constructor(saucer: Saucer) : this(saucer.position, 2.0, ColorRGBa.GREEN, saucer.velocity*0.5)
     constructor(asteroid: Asteroid) : this(asteroid.position, 2.0, ColorRGBa.WHITE, asteroid.velocity*0.5)
 
+    override val collisionStrategy: Collider
+        get() = this
+    override val killRadius: Double
+        get() = 0.0
     var elapsedTime = 0.0
     private val lifetime = U.SPLAT_LIFETIME
     private val view = SplatView(lifetime)
+
+    override fun interactWith(other: Collidable, trans: Transaction) = Unit
+    override fun interact(asteroid: Asteroid, trans: Transaction) = Unit
+    override fun interact(missile: Missile, trans: Transaction) = Unit
+    override fun interact(saucer: Saucer, trans: Transaction) = Unit
+    override fun interact(ship: Ship, trans: Transaction) = Unit
 
     override fun update(deltaTime: Double, trans: Transaction) {
         elapsedTime += deltaTime
