3130e6fac0205164b30c6c1a059047aee20d1638
Ron Jeffries <ronjeffries@acm.org>
2023-02-03 11:18:46 -0500

    adjust interfaces Collider and SpaceObject, remove overlap, SpaceObject inherits Collider.

--- src/main/kotlin/com/ronjeffries/ship/SpaceObject.kt
+++ src/main/kotlin/com/ronjeffries/ship/SpaceObject.kt
@@ -2,13 +2,7 @@ package com.ronjeffries.ship
 
 import org.openrndr.draw.Drawer
 
-interface SpaceObject {
-    val position: Point
-    val killRadius: Double
-    fun interact(asteroid: Asteroid, trans: Transaction)
-    fun interact(missile: Missile, trans: Transaction)
-    fun interact(saucer: Saucer, trans: Transaction)
-    fun interact(ship: Ship, trans: Transaction)
+interface SpaceObject: Collider {
     fun interactWith(other: SpaceObject, trans: Transaction)
     fun draw(drawer: Drawer)
     fun update(deltaTime: Double, trans: Transaction)
